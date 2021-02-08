9771efd532a12dc96f5c6a155f4ddca1b9576736
abubaker <mohammad_abubaker@yahoo.com>
2021-02-08 18:39:54 +0500

    Created @Entity for Table

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -14,5 +14,28 @@
  * limitations under the License.
  */
 
+/**
+ * Step 1 - Creating Entity for Tables in SQLite
+ */
 package com.example.android.trackmysleepquality.database
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "daily_sleep_quality_table")
+data class SleepNight(
+
+        @PrimaryKey(autoGenerate = true)
+        var nightId: Long = 0L,
+
+        @ColumnInfo(name = "start_time_milli")
+        val startTimeMilli: Long = System.currentTimeMillis(),
+
+        @ColumnInfo(name = "end_time_milli")
+        val endTimeMill: Long = startTimeMilli,
+
+        @ColumnInfo(name = "quality_sleep")
+        var sleepQuality: Int = -1
+
+)
