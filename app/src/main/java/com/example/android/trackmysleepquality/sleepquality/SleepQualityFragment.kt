4292c3dc4b56da4f4dffbefdd78c2cb6dcc93898
abubaker <mohammad_abubaker@yahoo.com>
2021-02-14 01:42:52 +0500

    Added comments in SleepQualityFragment.kt

--- app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityFragment.kt
@@ -51,19 +51,47 @@ class SleepQualityFragment : Fragment() {
 
         val application = requireNotNull(this.activity).application
 
-
+        // We need to retrieve arguments sent from SleepTrackerFragment using SafeArgs to
+        // SleepQualityFragment using SleepQualityFragmentArgs and extracting from the Bundle:
         val arguments = SleepQualityFragmentArgs.fromBundle(requireArguments())
 
-
+        // Reference to our DAO file (implementation of CRUD functions)
         val dataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
 
+        // Build Factory based on:
+        // 1. Unique Key (sleepNightKey)
+        // 2. Reference to DAO File for database
         val viewModelFactory = SleepQualityViewModelFactory(arguments.sleepNightKey, dataSource)
 
+        // Retrieves a selected entry based on the provided Primary Key
+        // Refer to: get() method in SleepDatabaseDao.kt file
         val sleepQualityViewModel = ViewModelProvider(this, viewModelFactory)
                 .get(SleepQualityViewModel::class.java)
 
+        // We will add the ViewModel to the binding object
         binding.sleepQualityViewModel = sleepQualityViewModel
 
+        /**
+         * Observer: navigateToSleepTracker
+         * The purpose of this observer is to collect and  store user's provided rating for the "sleep quality"
+         *
+         * After completing this function:
+         *
+         * 1. Populate <variable> data in the fragment_sleep_quality.xml for
+         *    connecting buttons onClick events with the Observer
+         *
+         * 2. Add following code in ImageViews which will act as icons for ranking sleep quality
+         *    android:onClick="@{() -> sleepQualityViewModel.onSetSleepQuality(0)}"
+         *
+         *    Note: .onSetSleepQuality(***) will have values from:
+         *    0 Very Bad
+         *    1 Poor
+         *    2 So so
+         *    3 OK
+         *    4 Pretty Good
+         *    5 Excellent
+         *
+         */
         sleepQualityViewModel.navigateToSleepTracker.observe(viewLifecycleOwner, Observer {
             if (it == true) { // Observed state is true.
                 this.findNavController().navigate(
